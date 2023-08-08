7f6b12c3325ed03e655a8713a24ba8a0c862dc6f
harvey_lin <harvey_lin@linkwish.com>
2023-08-08 17:44:47 +0800

    調整modal

--- dist/js/vue-global-component.js
+++ dist/js/vue-global-component.js
@@ -269,17 +269,17 @@ Vue.component('tip-modal', {
 })
 Vue.component('app-download-modal', {
   template: `
-    <div class="modal fade appDownloadPopup" id="appDownloadPopup" data-backdrop="static">
+    <div id="appDownloadPopup" class="modal fade" data-backdrop="static">
       <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
-          <div class="modal-header justify-content-center">
+          <div class="modal-header">
             <h2>APP下載</h2>
           </div>
           <div class="modal-body">
             <div class="qrcode mx-auto"></div>
           </div>
-          <div class="modal-footer">
-            <button class="btn btn-limeGreen mx-auto limit" data-dismiss="modal">確認</button>
+          <div class="justify-content-center modal-footer">
+            <button class="btn btn-a" data-dismiss="modal">確認</button>
           </div>
         </div>
       </div>
