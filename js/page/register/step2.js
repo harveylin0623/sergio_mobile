c8cb3404fdf7bc263061511469a01218aaedbec9
harvey_lin <harvey_lin@linkwish.com>
2023-08-22 09:58:27 +0800

    完成註冊第二部修改

--- js/page/register/step2.js
+++ js/page/register/step2.js
@@ -1,93 +1,93 @@
 export default function({ apiUrl, pageUrl }) {
-  let widgetId = null
   new Vue({
     el: '#app',
-    mixins: [encodePhoneTextMixin],
+    mixins: [encodePhoneTextMixin, signUpStepMixin],
     store: window.myVuexStore,
     data: {
-      user: { mobile: '', temp_access_token: '', verify_code: '', recaptcha_token: '' },
-      smsInfo: { status: false, message: '' },
-      tipInfo: { statue: false , message: ''},
+      user: { mobile: '', temp_access_token: '', verify_code: '' },
+      tipInfo: { statue: false , message: '', isOpen: false },
+      actionType: '',
+      storageKey: 'register',
       isLoading: false,
       apiUrl,
       pageUrl
     },
     methods: {
       checkHasUserData() {
-        let storageData = window.sessionStorageObj.getItem('register')
+        let storageData = window.sessionStorageObj.getItem(this.storageKey)
         let hasData = storageData !== null
         if (hasData) {
           this.user.mobile = storageData.payload.user.mobile
           this.user.temp_access_token = storageData.temp_access_token
         } else {
-          $('#refillPopup').modal('show')
+          this.actionType = 'refill'
+          this.errorHandler('請重新填寫表單')
         }
         return hasData
       },
-      initRecaptcha() {
-        grecaptcha.ready(() => {
-          widgetId = grecaptcha.render('recaptcha-container', {
-            sitekey: '6LfqA68UAAAAAHivfnF1r5rAYcZn2hAnnjGtd2US',
-            callback: (token) => {
-              this.user.recaptcha_token = token
-            }
-          })
-        })
-      },
-      checkRecaptchaToken() {
-        return authApi.recaptcha({
-          url: apiUrl.verify_google_recaptcha,
-          data: { token: this.user.recaptcha_token }
-        })
-      },
       async sendToMobile() {
         this.isLoading = true
+        this.actionType = 'sms'
         let response = await authApi.resend_register_verify({
           url: apiUrl.resend_register_verify,
           data: { temp_access_token: this.user.temp_access_token }
         })
-        this.smsInfo.status = response.status === 1
-        this.smsInfo.message = response.message
-        if (!this.smsInfo.status) window.sessionStorageObj.removeItem('register')
-        $('#smsPopup').modal('show')
+        if (response.status === 1) {
+          this.tipInfo.status = true
+          this.tipInfo.message = response.message
+          this.tipInfo.isOpen = true
+        } else {
+          window.sessionStorageObj.removeItem(this.storageKey)
+          this.errorHandler(response.message)
+        }
         this.isLoading = false
       },
-      registerVerify() {
-        return authApi.register_verify({
+      async submitHandler() {
+        let isValid = await this.$refs.form.validate()
+        if (!isValid) return
+        this.isLoading = true
+        this.actionType = 'verify'
+        document.activeElement.blur()
+        let response = await authApi.register_verify({
           url: apiUrl.register_verify,
           data: {
             temp_access_token: this.user.temp_access_token,
             verify_code: this.user.verify_code
           }
         })
-      },
-      async submitHandler() {
-        let isValid = await this.$refs.form.validate()
-        if (!isValid) return
-        this.isLoading = true
-        // let { success } = await this.checkRecaptchaToken()
-        // if (!success) return this.errorHandler('recaptcha驗證有誤,請重新驗證')
-        document.activeElement.blur()
-        let response = await this.registerVerify()
         let callback = response.status === 1 ? this.successHandler : this.errorHandler
         callback(response.message)
       },
       successHandler(message) {
         this.tipInfo.status = true
         this.tipInfo.message = message
-        window.sessionStorageObj.removeItem('register')
-        $('#tipPopup').modal('show')
+        this.tipInfo.isOpen = true
+        window.sessionStorageObj.removeItem(this.storageKey)
         this.isLoading = false
       },
       errorHandler(message) {
-        // grecaptcha.reset(widgetId)
         this.tipInfo.status = false
         this.tipInfo.message = message
-        $('#tipPopup').modal('show')
+        this.tipInfo.isOpen = true
         this.isLoading = false
+      },
+      confirmHandler() {
+        const { register_step1, login } = this.pageUrl
+        switch (this.actionType) {
+          case 'refill':
+            location.href = register_step1
+            break
+          case 'sms':
+            if (!this.tipInfo.status) location.href = register_step1
+            break
+          case 'verify':
+            if (this.tipInfo.status) location.href = login
+            break
+        }
       }
     },
     mounted() {
+      this.stepList[0].done = true
       this.checkHasUserData()
     }
   })
