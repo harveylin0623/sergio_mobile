67e34ca9f7b969b06ac6680be4b200a21d9c82ea
harvey_lin <harvey_lin@linkwish.com>
2023-08-22 11:09:58 +0800

    完成忘記密碼第二步

--- js/page/forgotPassword/step2.js
+++ js/page/forgotPassword/step2.js
@@ -5,39 +5,48 @@ export default function({ apiUrl, pageUrl }) {
     store: window.myVuexStore,
     data: {
       user: { mobile: '', temp_access_token: '', verify_code: '' },
-      smsInfo: { status: false, message: '' },
-      tipInfo: { status: false, message: '' },
+      tipInfo: { status: false, message: '', isOpen: false },
+      actionType: '',
+      storageKey: 'forgetPw',
       isLoading: false,
       apiUrl,
       pageUrl
     },
     methods: {
       checkHasUserData() {
-        let storageData = window.sessionStorageObj.getItem('forgetPw')
+        let storageData = window.sessionStorageObj.getItem(this.storageKey)
         let hasData = storageData !== null
         if (hasData) {
           this.user.mobile = storageData.mobile
           this.user.temp_access_token = storageData.temp_access_token
         } else {
-          $('#refillPopup').modal('show')
+          this.actionType = 'refill'
+          this.errorHandler('請重新填寫表單')
         }
         return hasData
       },
       async sendToMobile() {
         this.isLoading = true
-        let response = await authApi.resend_forget_verify({
+        this.actionType = 'sms'
+        let response = await authApi.resend_register_verify({
           url: apiUrl.resend_forget_verify,
           data: { temp_access_token: this.user.temp_access_token }
         })
-        this.smsInfo.status = response.status === 1
-        this.smsInfo.message = response.message
-        if (this.smsInfo.status) $('#smsPopup').modal('show')
+        if (response.status === 1) {
+          this.tipInfo.status = true
+          this.tipInfo.message = response.message
+          this.tipInfo.isOpen = true
+        } else {
+          window.sessionStorageObj.removeItem(this.storageKey)
+          this.errorHandler(response.message)
+        }
         this.isLoading = false
       },
       async submitHandler() {
         let isValid = await this.$refs.form.validate()
         if (!isValid) return
         this.isLoading = true
+        this.actionType = 'verify'
         document.activeElement.blur()
         let response = await authApi.forget_password_verify({
           url: apiUrl.forget_password_verify,
@@ -48,8 +57,22 @@ export default function({ apiUrl, pageUrl }) {
         })
         this.tipInfo.status = response.status === 1
         this.tipInfo.message = response.message
-        $('#tipPopup').modal('show')
+        this.tipInfo.isOpen = true
         this.isLoading = false
+      },
+      confirmHandler() {
+        const { forget_password_step1, forget_password_step3 } = this.pageUrl
+        switch (this.actionType) {
+          case 'refill':
+            location.href = forget_password_step1
+            break
+          case 'sms':
+            if (!this.tipInfo.status) location.href = forget_password_step1
+            break
+          case 'verify':
+            if (this.tipInfo.status) location.href = forget_password_step3
+            break
+        }
       }
     },
     mounted() {
